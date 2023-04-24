79f8535d5f17c44ba3400256d6fff32a39854f58
Ryan Kent <imsolost@gmail.com>
2023-04-23 20:55:42 -0700

    copy to clipboard

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -11,6 +11,7 @@ const Demo = () => {
 
   const [allArticles, setAllArticles] = useState([])
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+  const [copied, setCopied] = useState('')
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
@@ -36,6 +37,12 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false, 3000))
   }
 
   return (
@@ -77,9 +84,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className='link_card'
             >
-              <div className='copy_btn'>
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt='copy_icon'
                   className='w-[40%] h-[40%] object-contain'
                 />
@@ -111,7 +118,9 @@ const Demo = () => {
                 Article <span className='blue_gradient'>Summary</span>
               </h2>
               <div className='summary_box'>
-                <p>{article.summary}</p>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
               </div>
             </div>
           )
